fab995f678d731310df0ecff9b8ffd70a8486de4
pangliang <418094911@qq.com>
2015-06-11 14:20:30 +0800

    创建项目

---
+++ gen/com/zhaoxiaodan/blog/androidantidebug/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.zhaoxiaodan.blog.androidantidebug;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
